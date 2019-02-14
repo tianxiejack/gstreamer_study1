5931fe6e1a8079778bcf2c04061a0ff962c2f425
alex <trackxt@qq.com>
2019-02-14 11:24:22 +0800

    2lesson

--- main.c
+++ main.c
@@ -5,58 +5,81 @@
  *      Author: jet
  */
 
-
 #include <gst/gst.h>
-
 
 int main(int argc, char *argv[])
 {
-#if 0
+	GstElement *pipeline, *source, *sink;
+	  GstBus *bus;
+	  GstMessage *msg;
+	  GstStateChangeReturn ret;
 
-	   const gchar *nano_str;
-	    guint major, minor, micro, nano;
-	    gst_init (&argc, &argv);
-	    gst_version (&major, &minor, &micro, &nano);
+	  /* Initialize GStreamer */
+	  gst_init (&argc, &argv);
 
-	    if (nano == 1)
-	        nano_str = "(CVS)";
-	    else if (nano == 2)
-	        nano_str = "(Prerelease)";
-	    else
-	        nano_str = "";
+	  /* Create the elements */
+	  source = gst_element_factory_make ("videotestsrc", "source");
+	  sink = gst_element_factory_make ("autovideosink", "sink");
 
-	    printf ("This program is linked against GStreamer %d.%d.%d %s\n",
-	          major, minor, micro, nano_str);
-	    return 0;
+	  /* Create the empty pipeline */
+	  pipeline = gst_pipeline_new ("test-pipeline");
 
-#else
+	  if (!pipeline || !source || !sink) {
+	    g_printerr ("Not all elements could be created.\n");
+	    return -1;
+	  }
 
-  GstElement *pipeline;
-  GstBus *bus;
-  GstMessage *msg;
+	  /* Build the pipeline */
+	  gst_bin_add_many (GST_BIN (pipeline), source, sink, NULL);
+	  if (gst_element_link (source, sink) != TRUE) {
+	    g_printerr ("Elements could not be linked.\n");
+	    gst_object_unref (pipeline);
+	    return -1;
+	  }
 
-  /* Initialize GStreamer */
-  gst_init (&argc, &argv);
+	  /* Modify the source's properties */
+	  g_object_set (source, "pattern", 0, NULL);
 
-  /* Build the pipeline */
-  pipeline = gst_parse_launch ("playbin uri=http://docs.gstreamer.com/media/sintel_trailer-480p.webm", NULL);
+	  /* Start playing */
+	  ret = gst_element_set_state (pipeline, GST_STATE_PLAYING);
+	  if (ret == GST_STATE_CHANGE_FAILURE) {
+	    g_printerr ("Unable to set the pipeline to the playing state.\n");
+	    gst_object_unref (pipeline);
+	    return -1;
+	  }
 
-  /* Start playing */
-  gst_element_set_state (pipeline, GST_STATE_PLAYING);
+	  /* Wait until error or EOS */
+	  bus = gst_element_get_bus (pipeline);
+	  msg = gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE, GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
 
-  /* Wait until error or EOS */
-  bus = gst_element_get_bus (pipeline);
+	  /* Parse message */
+	  if (msg != NULL) {
+	    GError *err;
+	    gchar *debug_info;
 
-  msg = gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE, GST_MESSAGE_EOS | GST_MESSAGE_ERROR );
+	    switch (GST_MESSAGE_TYPE (msg)) {
+	      case GST_MESSAGE_ERROR:
+	        gst_message_parse_error (msg, &err, &debug_info);
+	        g_printerr ("Error received from element %s: %s\n", GST_OBJECT_NAME (msg->src), err->message);
+	        g_printerr ("Debugging information: %s\n", debug_info ? debug_info : "none");
+	        g_clear_error (&err);
+	        g_free (debug_info);
+	        break;
+	      case GST_MESSAGE_EOS:
+	        g_print ("End-Of-Stream reached.\n");
+	        break;
+	      default:
+	        /* We should not reach here because we only asked for ERRORs and EOS */
+	        g_printerr ("Unexpected message received.\n");
+	        break;
+	    }
+	    gst_message_unref (msg);
+	  }
 
-  /* Free resources */
-  if (msg != NULL)
-    gst_message_unref (msg);
-  gst_object_unref (bus);
-  gst_element_set_state (pipeline, GST_STATE_NULL);
-  gst_object_unref (pipeline);
-  return 0;
-
-#endif
+	  /* Free resources */
+	  gst_object_unref (bus);
+	  gst_element_set_state (pipeline, GST_STATE_NULL);
+	  gst_object_unref (pipeline);
+	  return 0;
 }
 
