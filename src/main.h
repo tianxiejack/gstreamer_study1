c111a830526ada2a2972319fd361d628e21030a8
alex <trackxt@qq.com>
2019-03-05 10:42:06 +0800

    01　hello world

---
+++ src/main.h
@@ -0,0 +1,17 @@
+/*
+ * main.h
+ *
+ *  Created on: 2019年3月5日
+ *      Author: jet
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#define M_MAIN main_01hello
+
+
+int main_01hello(int argc, char **argv);
+
+
+#endif /* MAIN_H_ */
