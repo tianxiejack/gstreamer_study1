68023d4d0b51e335625292d7881612082f3613f5
alex <alex@qr.com>
2019-09-20 18:47:51 +0800

    02

--- src/main.h
+++ src/main.h
@@ -8,10 +8,12 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-#define M_MAIN main_01hello
+#define M_MAIN main_02
 
 
 int main_01hello(int argc, char **argv);
+int main_02(int argc, char *argv[]) ;
+
 
 
 #endif /* MAIN_H_ */
--- src/main_01hello.c
+++ src/main_01hello.c
@@ -17,7 +17,10 @@ int main_01hello(int argc, char *argv[]) {
   gst_init (&argc, &argv);
 
   /* Build the pipeline */
-  pipeline = gst_parse_launch ("playbin uri=http://docs.gstreamer.com/media/sintel_trailer-480p.webm", NULL);
+  //pipeline = gst_parse_launch ("playbin uri=http://docs.gstreamer.com/media/sintel_trailer-480p.webm", NULL);
+	
+  //pipeline = gst_parse_launch ("playbin uri=file:///home/nvidia/Videos/MOV.mp4", NULL);
+  pipeline = gst_parse_launch ("playbin uri=file:///home/alex/videosrc/MOV.mp4", NULL);
 
   /* Start playing */
   gst_element_set_state (pipeline, GST_STATE_PLAYING);
